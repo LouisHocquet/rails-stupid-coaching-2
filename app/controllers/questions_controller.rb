c53b785598df3bd190dde28fbdbae0357f5d66af
Louis Hocquet <louis.hocquet@sfr.fr>
2023-08-26 12:05:14 +0200

    Added tests system and screenshots to stupid coacjing ror

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,12 +6,8 @@ class QuestionsController < ApplicationController
 
   def answer
     @your_question = params[:question]
-    if @your_question.capitalize.match?('I am going to work right now!')
-      @coach_answer = "That's great son!"
-    else
-      @coach_answer = "I don't care, get dressed and go to work!"
-      @coach_answer = 'Silly question, get dressed and go to work!' if @your_question.capitalize.match?(/\?$/)
-      @coach_answer = "I can feel your motivation! #{@coach_answer}" if @your_question == @your_question.upcase
-    end
+    @coach_answer = "I don't care, get dressed and go to work!"
+    @coach_answer = 'Great!' if @your_question.capitalize.match?('I am going to work')
+    @coach_answer = 'Silly question, get dressed and go to work!' if @your_question.capitalize.match?(/\?$/)
   end
 end
